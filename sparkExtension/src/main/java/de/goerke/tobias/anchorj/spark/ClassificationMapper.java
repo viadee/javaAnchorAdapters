b74e40bd1c6dedcdb8aaa14a0ebab9c75e56a7f8
Tobias Goerke <tobias.goerke@gmail.com>
2018-11-11 15:23:29 +0100

    Initial commit. Added spark module

---
+++ sparkExtension/src/main/java/de/goerke/tobias/anchorj/spark/ClassificationMapper.java
@@ -0,0 +1,34 @@
+package de.goerke.tobias.anchorj.spark;
+
+import de.goerke.tobias.anchorj.base.ClassificationFunction;
+import de.goerke.tobias.anchorj.base.DataInstance;
+import org.apache.spark.api.java.function.PairFunction;
+import scala.Tuple2;
+
+/**
+ * This class is used in conjunction with Apache Spark.
+ * <p>
+ * As Spark expects serializable objects, this "hack" is necessary when dealing with data that cannot easily be
+ * serialized.
+ * <p>
+ * This class in particular maps each candidate to a tuple of candidate plus its prediction.
+ *
+ * @param <T> Type of the instance
+ */
+class ClassificationMapper<T extends DataInstance<?>> implements PairFunction<T, T, Integer> {
+    private static ClassificationFunction classificationFunction;
+
+    /**
+     * @param classificationFunction the classificationFunction used to predict the label
+     */
+    @SuppressWarnings("static-access")
+    ClassificationMapper(ClassificationFunction<T> classificationFunction) {
+        this.classificationFunction = classificationFunction;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public Tuple2<T, Integer> call(T t) {
+        return new Tuple2<>(t, classificationFunction.predict(t));
+    }
+}
