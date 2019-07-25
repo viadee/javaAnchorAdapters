74e41146a20659a0d3204abb4724bbdaa3190ea6
Tobias Goerke <tobias.goerke@gmail.com>
2019-07-25 12:01:11 +0200

    Updated boolean transfomer to accept numerics

--- DefaultConfigsExtension/src/main/java/de/viadee/xai/anchor/adapter/tabular/transformations/StringToBooleanTransformer.java
+++ DefaultConfigsExtension/src/main/java/de/viadee/xai/anchor/adapter/tabular/transformations/StringToBooleanTransformer.java
@@ -3,9 +3,20 @@ package de.viadee.xai.anchor.adapter.tabular.transformations;
 import java.io.Serializable;
 
 /**
- * Transforms TRUE/FALSE values to Boolean types
+ * Transforms TRUE/FALSE or numeric values to Boolean types
+ * <p>
+ * Numeric values are converted to FALSE, when they equal 0, otherwise they are true
  */
-public class StringToBooleanTransformer implements Transformer{
+public class StringToBooleanTransformer implements Transformer {
+
+    private static boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 
     @Override
     public Boolean apply(Serializable serializable) {
@@ -13,9 +24,18 @@ public class StringToBooleanTransformer implements Transformer{
     }
 
     private boolean tryConvertToBoolean(Serializable serializable) throws NumberFormatException {
-        String value = serializable.toString().toLowerCase();
-        if (!Boolean.TRUE.toString().equals(value) && !Boolean.FALSE.toString().equals(value))
+        final String value = serializable.toString().toLowerCase();
+        Double numeric;
+        try {
+            numeric = Double.parseDouble(value);
+        } catch (NumberFormatException e) {
+            numeric = null;
+        }
+        if ((!Boolean.TRUE.toString().equals(value) && !Boolean.FALSE.toString().equals(value)) &&
+                numeric == null)
             throw new IllegalArgumentException("Value is not a boolean");
+        if (numeric != null)
+            return numeric != 0D;
         return Boolean.valueOf(String.valueOf(serializable));
     }
 }
