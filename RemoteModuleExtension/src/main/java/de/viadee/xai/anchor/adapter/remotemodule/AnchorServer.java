79e823fc83cdb60f622c6a9a537a007f8016d7f1
Tobias Goerke <tobias.goerke@gmail.com>
2019-08-02 15:38:57 +0200

    Added parameters Setup maven jar build

--- RemoteModuleExtension/src/main/java/de/viadee/xai/anchor/adapter/remotemodule/AnchorServer.java
+++ RemoteModuleExtension/src/main/java/de/viadee/xai/anchor/adapter/remotemodule/AnchorServer.java
@@ -2,6 +2,7 @@ package de.viadee.xai.anchor.adapter.remotemodule;
 
 import de.viadee.xai.anchor.algorithm.AnchorConstructionBuilder;
 import de.viadee.xai.anchor.algorithm.AnchorResult;
+import de.viadee.xai.anchor.algorithm.exploration.KL_LUCB;
 import org.apache.commons.cli.*;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -16,7 +17,6 @@ import java.net.SocketTimeoutException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * Contains code that enables third parties to access the Java implementation by using inter-process communication
@@ -30,17 +30,43 @@ public class AnchorServer {
      */
     public static void main(String[] args) throws IOException {
         try {
-            final Options options = new Options();
+            final Map<String, Option> optionsMap = new HashMap<>();
 
-            final Option portOption = new Option("p", "port", true, "The socket connection port " +
-                    "used to listen to incoming data");
-            portOption.setRequired(true);
-
-            final Option timeoutOption = new Option("to", "timeout", true, "The number of seconds " +
+            optionsMap.put("port", new Option("p", "port", true,
+                    "The socket connection port used to listen to incoming data"));
+            optionsMap.put("timeout", new Option("to", "timeout", true, "The number of seconds " +
                     "the server will listen after each last communications act. Set 0 to disable automatic shutdown. " +
-                    "Default = 60 seconds. ");
+                    "Default = 60 seconds. "));
+            optionsMap.put("maxAnchorSize", new Option("maxAnchor", "maxAnchorSize", true,
+                    "Max combined features of the resulting anchor."));
+            optionsMap.put("beamSize", new Option("beam", "beamSize", true,
+                    "Parameter B: size of the current candidates for beam search."));
+            optionsMap.put("delta", new Option("delta", "delta", true,
+                    "The delta value describing the probability of identifying the best arm confidence."));
+            optionsMap.put("epsilon", new Option("epsilon", "epsilon", true,
+                    "The maximum tolerated error == tolerance."));
+            optionsMap.put("tau", new Option("tau", "tau", true,
+                    "The desired precision an anchor needs to achieve. If no candidate achieves at least " +
+                            "this precision, the one with the best precision will be returned"));
+            optionsMap.put("tauDiscrepancy", new Option("tauD", "tauDiscrepancy", true,
+                    "Usually, it is practically infeasible to sample until the mean and the upper/lower " +
+                            "bounds simultaneously fall below or above the tau threshold. Therefore, this variable " +
+                            "may be introduced to control this discrepancy."));
+            optionsMap.put("initSampleCount", new Option("initS", "initSampleCount", true,
+                    "The number of evaluations sampled for each candidate before it gets evaluated by " +
+                            "the best arm identification algorithm. While theoretically, a guarantee that no " +
+                            "candidates get discarded due to too few samples is provided by delta, using this " +
+                            "argument has practical advantages."));
+            optionsMap.put("allowSuboptimalSteps", new Option("subSteps", "allowSuboptimalSteps", true,
+                    "If set to false, candidates that are returned by the best arm identification get " +
+                            "removed when their precision is lower than their parent's"));
+            optionsMap.put("batchSize", new Option("batch", "batchSize", true,
+                    "The number of evaluations to perform on each pull of the arm."));
 
-            Stream.of(portOption, timeoutOption).forEach(options::addOption);
+            optionsMap.get("port").setRequired(true);
+
+            final Options options = new Options();
+            optionsMap.values().forEach(options::addOption);
 
             final CommandLineParser parser = new DefaultParser();
             final HelpFormatter formatter = new HelpFormatter();
@@ -53,36 +79,50 @@ public class AnchorServer {
                 throw new RuntimeException(e);
             }
 
-            int portNr = 6666;
-            int timeout = 60;
+            final Map<String, Object> optionValueMap = new HashMap<>();
             try {
-                portNr = Integer.parseInt(cmd.getOptionValue(portOption.getOpt()));
-                timeout = Integer.parseInt(cmd.getOptionValue(timeoutOption.getLongOpt(), "60"));
+                putValue(cmd, optionsMap, optionValueMap, "port", null, 'i');
+                putValue(cmd, optionsMap, optionValueMap, "timeout", "60", 'i');
+                putValue(cmd, optionsMap, optionValueMap, "maxAnchorSize", "0", 'i');
+                putValue(cmd, optionsMap, optionValueMap, "beamSize", "2", 'i');
+                putValue(cmd, optionsMap, optionValueMap, "delta", "0.9", 'd');
+                putValue(cmd, optionsMap, optionValueMap, "epsilon", "0.05", 'd');
+                putValue(cmd, optionsMap, optionValueMap, "tau", "0.9", 'd');
+                putValue(cmd, optionsMap, optionValueMap, "tauDiscrepancy", "0.05", 'd');
+                putValue(cmd, optionsMap, optionValueMap, "initSampleCount", "20", 'i');
+                putValue(cmd, optionsMap, optionValueMap, "allowSuboptimalSteps", "true", 'b');
+                putValue(cmd, optionsMap, optionValueMap, "batchSize", "100", 'i');
             } catch (NumberFormatException e) {
                 throw new RuntimeException("Invalid option value", e);
             }
 
-            final Map<Option, Object> optionMap = new HashMap<>();
-            optionMap.put(portOption, portNr);
-            optionMap.put(timeoutOption, timeout);
-
             System.out.println(
                     "Starting Server with following params:" + System.lineSeparator() +
-                            optionMap.entrySet().stream()
-                                    .map(e -> "\t" + e.getKey().getLongOpt() + ": " + e.getValue())
+                            optionValueMap.entrySet().stream()
+                                    .map(e -> "\t" + e.getKey() + ": " + e.getValue())
                                     .collect(Collectors.joining(System.lineSeparator())));
 
             try {
-                try (final ServerSocket listener = new ServerSocket(portNr)) {
-                    if (timeout > 0)
-                        listener.setSoTimeout(timeout * 1000);
+                try (final ServerSocket listener = new ServerSocket((int) optionValueMap.get("port"))) {
+                    if (((int) optionValueMap.get("timeout")) > 0)
+                        listener.setSoTimeout((int) optionValueMap.get("timeout") * 1000);
                     // TODO this is dangerous. Concurrent processes will answer to same algorithm thread.
                     //  evaluate ID and relay to correct instance before enabling threading!!
                     //  Furthermore, it is unclear whether threading using serversockets on a local instance would work
                     //  anyway
                     //final ExecutorService pool = Executors.newFixedThreadPool(1);
                     while (true) {
-                        new AnchorService(listener.accept());
+                        new AnchorService(
+                                listener.accept(),
+                                (int) optionValueMap.get("maxAnchorSize"),
+                                (int) optionValueMap.get("beamSize"),
+                                (double) optionValueMap.get("delta"),
+                                (double) optionValueMap.get("epsilon"),
+                                (double) optionValueMap.get("tau"),
+                                (double) optionValueMap.get("tauDiscrepancy"),
+                                (int) optionValueMap.get("initSampleCount"),
+                                (boolean) optionValueMap.get("allowSuboptimalSteps"),
+                                (int) optionValueMap.get("batchSize"));
                     }
                 }
             } catch (SocketTimeoutException e) {
@@ -95,11 +135,55 @@ public class AnchorServer {
         }
     }
 
+    private static void putValue(CommandLine cmd, Map<String, Option> optionsMap, Map<String, Object> resultMap,
+                                 String optionKey, String defaultValue, char type) {
+        final Option option = optionsMap.get(optionKey);
+        final String optionValue = (defaultValue != null)
+                ? cmd.getOptionValue(option.getOpt(), defaultValue)
+                : cmd.getOptionValue(optionsMap.get(optionKey).getOpt());
+
+        Object targetValue;
+        switch (type) {
+            case 'i':
+                targetValue = Integer.parseInt(optionValue);
+                break;
+            case 'd':
+                targetValue = Double.parseDouble(optionValue);
+                break;
+            case 'b':
+                targetValue = Boolean.parseBoolean(optionValue);
+                break;
+            default:
+                throw new RuntimeException("Unexpected type");
+        }
+        resultMap.put(option.getLongOpt(), targetValue);
+    }
+
     private static class AnchorService implements Runnable {
         private final Socket socket;
 
-        private AnchorService(final Socket socket) {
+        private final int maxAnchorSize;
+        private final int beamSize;
+        private final double delta;
+        private final double epsilon;
+        private final double tau;
+        private final double tauDiscrepancy;
+        private final int initSampleCount;
+        private final boolean allowSuboptimalSteps;
+        private final int batchSize;
+
+        private AnchorService(Socket socket, int maxAnchorSize, int beamSize, double delta, double epsilon, double tau,
+                              double tauDiscrepancy, int initSampleCount, boolean allowSuboptimalSteps, int batchSize) {
             this.socket = socket;
+            this.maxAnchorSize = maxAnchorSize;
+            this.beamSize = beamSize;
+            this.delta = delta;
+            this.epsilon = epsilon;
+            this.tau = tau;
+            this.tauDiscrepancy = tauDiscrepancy;
+            this.initSampleCount = initSampleCount;
+            this.allowSuboptimalSteps = allowSuboptimalSteps;
+            this.batchSize = batchSize;
         }
 
         @Override
@@ -132,14 +216,23 @@ public class AnchorServer {
                         final RemoteCallbackCoverageIdentification coverageIdentification =
                                 new RemoteCallbackCoverageIdentification(in, out);
 
-                        final AnchorResult anchorResult = new AnchorConstructionBuilder<>(
-                                remoteCallbackSamplingFunction, instance, 0)
-                                .setCoverageIdentification(coverageIdentification)
-                                .setLazyCoverageEvaluation(true)
-                                .setTau(0.95)
-                                .setTauDiscrepancy(0.1)
-                                .build().constructAnchor();
+                        final AnchorConstructionBuilder<RemoteInstance> remoteInstanceAnchorConstructionBuilder =
+                                new AnchorConstructionBuilder<>(
+                                        remoteCallbackSamplingFunction, instance, 0)
+                                        .setCoverageIdentification(coverageIdentification)
+                                        .setBeamSize(beamSize)
+                                        .setDelta(delta)
+                                        .setEpsilon(epsilon)
+                                        .setTau(tau)
+                                        .setTauDiscrepancy(tauDiscrepancy)
+                                        .setInitSampleCount(initSampleCount)
+                                        .setAllowSuboptimalSteps(allowSuboptimalSteps)
+                                        .setBestAnchorIdentification(new KL_LUCB(batchSize));
+                        if (maxAnchorSize > 0)
+                            remoteInstanceAnchorConstructionBuilder.setMaxAnchorSize(maxAnchorSize);
 
+                        final AnchorResult anchorResult = remoteInstanceAnchorConstructionBuilder
+                                .build().constructAnchor();
                         //send response
                         final JSONObject response = new JSONObject();
                         response.append("id", id);
