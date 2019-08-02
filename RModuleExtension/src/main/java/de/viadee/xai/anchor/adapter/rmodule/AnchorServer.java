6e59175c008aa062d690b3220e89dbe6cdc840ab
Tobias Goerke <tobias.goerke@gmail.com>
2019-08-02 14:14:38 +0200

    Commented, formatted and refactored rmodule

--- RModuleExtension/src/main/java/de/viadee/xai/anchor/adapter/rmodule/AnchorServer.java
+++ RModuleExtension/src/main/java/de/viadee/xai/anchor/adapter/rmodule/AnchorServer.java
@@ -2,9 +2,9 @@ package de.viadee.xai.anchor.adapter.rmodule;
 
 import de.viadee.xai.anchor.algorithm.AnchorConstructionBuilder;
 import de.viadee.xai.anchor.algorithm.AnchorResult;
-import de.viadee.xai.anchor.algorithm.coverage.DisabledCoverageIdentification;
-import org.json.JSONObject;
 import org.apache.commons.cli.*;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -12,118 +12,160 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.net.SocketTimeoutException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+/**
+ * Contains code that enables third parties to access the Java implementation by using inter-process communication
+ */
 public class AnchorServer {
 
-    public static void main(String[] args) throws Exception {
-
-        Options options = new Options();
-
-        Option port = new Option("p", "port", true, "port for socket connection");
-        port.setRequired(true);
-        options.addOption(port);
-
-        CommandLineParser parser = new DefaultParser();
-        HelpFormatter formatter = new HelpFormatter();
-        CommandLine cmd = null;
-
+    /**
+     * Starts the anchors server
+     *
+     * @param args arguments
+     * @throws IOException in case the Socket cannot be opened
+     */
+    public static void main(String[] args) throws IOException {
         try {
-            cmd = parser.parse(options, args);
-        } catch (ParseException e) {
-            System.out.println(e.getMessage());
-            formatter.printHelp("utility-name", options);
+            final Options options = new Options();
 
-            System.exit(1);
-        }
+            final Option portOption = new Option("p", "port", true, "The socket connection port " +
+                    "used to listen to incoming data");
+            portOption.setRequired(true);
 
-        int portNr = Integer.parseInt(cmd.getOptionValue("port"));
+            final Option timeoutOption = new Option("to", "timeout", true, "The number of seconds " +
+                    "the server will listen after each last communications act. Set 0 to disable automatic shutdown. " +
+                    "Default = 60 seconds. ");
 
-        try (ServerSocket listener = new ServerSocket(portNr)) {
-            System.out.println("Anchor server is running...");
-            ExecutorService pool = Executors.newFixedThreadPool(4);
-            while (true) {
-                pool.execute(new AnchorService(listener.accept()));
+            Stream.of(portOption, timeoutOption).forEach(options::addOption);
+
+            final CommandLineParser parser = new DefaultParser();
+            final HelpFormatter formatter = new HelpFormatter();
+            CommandLine cmd;
+
+            try {
+                cmd = parser.parse(options, args);
+            } catch (ParseException e) {
+                formatter.printHelp("utility-name", options);
+                throw new RuntimeException(e);
             }
+
+            int portNr = 6666;
+            int timeout = 60;
+            try {
+                portNr = Integer.parseInt(cmd.getOptionValue(portOption.getOpt()));
+                timeout = Integer.parseInt(cmd.getOptionValue(timeoutOption.getLongOpt(), "60"));
+            } catch (NumberFormatException e) {
+                throw new RuntimeException("Invalid option value", e);
+            }
+
+            final Map<Option, Object> optionMap = new HashMap<>();
+            optionMap.put(portOption, portNr);
+            optionMap.put(timeoutOption, timeout);
+
+            System.out.println(
+                    "Starting Server with following params:" + System.lineSeparator() +
+                            optionMap.entrySet().stream()
+                                    .map(e -> "\t" + e.getKey().getLongOpt() + ": " + e.getValue())
+                                    .collect(Collectors.joining(System.lineSeparator())));
+
+            try {
+                try (final ServerSocket listener = new ServerSocket(portNr)) {
+                    if (timeout > 0)
+                        listener.setSoTimeout(timeout * 1000);
+                    // TODO this is dangerous. Concurrent processes will answer to same algorithm thread.
+                    //  evaluate ID and relay to correct instance before enabling threading!!
+                    //  Furthermore, it is unclear whether threading using serversockets on a local instance would work
+                    //  anyway
+                    //final ExecutorService pool = Executors.newFixedThreadPool(1);
+                    while (true) {
+                        new AnchorService(listener.accept());
+                    }
+                }
+            } catch (SocketTimeoutException e) {
+                throw new RuntimeException("The connection has timed out. Stopping listening processes.");
+            }
+        } catch (Throwable e) {
+            System.out.println("Shutting down server due to exception: " + e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
         }
     }
 
     private static class AnchorService implements Runnable {
-        private Socket socket;
+        private final Socket socket;
 
-        AnchorService(Socket socket) {
+        private AnchorService(final Socket socket) {
             this.socket = socket;
         }
 
         @Override
         public void run() {
             System.out.println("Connected: " + socket);
-            String input, output;
             boolean quit = false;
-            RInstance instance;
             try {
-                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                final BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                final PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 
                 while (true) {
-                    if ((input = in.readLine()) != null) {
-                        try {
-                            JSONObject obj = new JSONObject(input);
+                    final String input = in.readLine();
+                    if (input == null)
+                        throw new RuntimeException("Empty response received");
+                    try {
+                        final JSONObject obj = new JSONObject(input);
 
-                            if (obj.has("quit") && obj.getInt("quit") == 1) {
-                                quit = true;
-                                return;
-                            }
+                        if (obj.has("quit") && obj.getInt("quit") == 1) {
+                            quit = true;
+                            return;
+                        }
 
-                            String id = obj.getString("id");
-                            int count = obj.getInt("count");
-                            String status = obj.getString("status");
-                            int dimension = obj.getInt("instance");
-                            // instance
-                            instance = new RInstance(dimension);
+                        final String id = obj.getString("id");
+                        final int count = obj.getInt("count");
+                        final int dimension = obj.getInt("instance");
+                        final RemoteInstance instance = new RemoteInstance(dimension);
 
+                        final RemoteCallbackSamplingFunction remoteCallbackSamplingFunction =
+                                new RemoteCallbackSamplingFunction(in, out);
+                        final RemoteCallbackCoverageIdentification coverageIdentification =
+                                new RemoteCallbackCoverageIdentification(in, out);
 
-                        AnchorResult anchorResult = new AnchorConstructionBuilder<>(
-                                new RCallbackSamplingFunction(out, in),
-                                instance,
-                                0
-                        )
-                                .setCoverageIdentification(new RCallbackCoverageIdentification(out, in))
+                        final AnchorResult anchorResult = new AnchorConstructionBuilder<>(
+                                remoteCallbackSamplingFunction, instance, 0)
+                                .setCoverageIdentification(coverageIdentification)
                                 .setLazyCoverageEvaluation(true)
                                 .setTau(0.95)
                                 .setTauDiscrepancy(0.1)
                                 .build().constructAnchor();
-                        // TODO set actual coverage ident fun
 
-                            //send response
-                            JSONObject response = new JSONObject();
-                            response.append("id", id);
-                            response.append("count", count + 1);
-                            response.append("status", "response");
-                            JSONObject anchorResultjson = new JSONObject(anchorResult);
-                            response.append("anchorResult", anchorResultjson);
-
-
-
-
-                            out.println(response.toString());
-                        } catch (Exception e){
-                            e.printStackTrace();
-                        }
-
+                        //send response
+                        final JSONObject response = new JSONObject();
+                        response.append("id", id);
+                        response.append("count", count + 1);
+                        response.append("status", "response");
+                        response.append("anchorResult", new JSONObject(anchorResult));
+                        out.println(response.toString());
+                    } catch (JSONException e) {
+                        throw new RuntimeException("Could not deserialize response");
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
                     }
                 }
             } catch (Exception e) {
-                System.out.println("Error:" + socket);
+                System.out.println("Error in AnchorService#run: " + socket);
+                throw new RuntimeException(e);
             } finally {
                 try {
                     socket.close();
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    throw new RuntimeException(e);
                 }
                 System.out.println("Closed: " + socket);
-                if (quit) System.exit(1);
+                if (quit)
+                    throw new RuntimeException("Server exit requested by client");
             }
         }
 
